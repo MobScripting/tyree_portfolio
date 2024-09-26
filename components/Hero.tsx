14c5499ba3f88c549dcd23aae4ee0bb0dcb58e97
Mob <mobscripting@gmail.com>
2024-09-26 12:40:58 -0400

    Continued work on landing page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Spotlight } from './ui/Spotlight'
 import { ModeToggle } from '@/app/darkbutton'
+import { TextGenerateEffect } from './ui/TextGenerateEffect'
 
 const Hero = () => {
   return (
@@ -20,10 +21,19 @@ const Hero = () => {
         </div>
 
         <div className='flex justify-center relative my-20 z-10'>
-            <div className='mas-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
+            <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
                 <h2 className='uppercase tracking-widest dark:text-blue-100 text-xs text-center text-black-100 max-w-80'>
                     Dynamic Web Magic with Next.js
                 </h2>
+
+                <TextGenerateEffect
+                    className='text-center text-[40px] md:text-5xl lg:text-6xl'
+                    words='Transforming Concepts into Seamless Experiences'
+                />
+
+                <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
+                    Hi, I&apos;m Tyree a Next.js Developer base in the United States
+                </p>
             </div>
         </div>
     </div>
